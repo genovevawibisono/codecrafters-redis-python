4713ba8066308f206edbbc57d36e9097bd2eb4bb
Oberyn <z5502369@ad.unsw.edu.au>
2025-08-30 09:42:37 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,13 @@
 import socket  # noqa: F401
+import threading
+
+def handle(connection):
+    while True:
+        data = connection.recv(1024)
+        if data == None:
+            break
+        connection.sendall(b"+PONG\r\n")
+    connection.close()
 
 
 def main():
@@ -8,14 +17,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-
     while True:
-        data = connection.recv(1024)
-        if data == None:
-            break
-        connection.sendall(b"+PONG\r\n")
-
+        connection, _ = server_socket.accept() # wait for client
+        client_thread = threading.Thread(target=handle, args=(connection,))
+        client_thread.start()
 
 if __name__ == "__main__":
     main()
